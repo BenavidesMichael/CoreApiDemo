1edb3c6e21ba1dbf8e0c1f21466822c688581557
Benavides Michael <Benavides.michael.munoz@gmail.com>
2023-01-12 23:14:47 +0100

    movies controller

---
+++ CoreApiDemoSolution/Infrastructure/SerilogExtention.cs
@@ -0,0 +1,18 @@
+﻿using Serilog;
+using Serilog.Formatting.Json;
+using System.Reflection;
+
+namespace CoreApiDemo.Infrastructure
+{
+    public static class SerilogExtention
+    {
+        public static IHostBuilder ConfigureSerilog(this IHostBuilder hostBuilder)
+        {
+            return hostBuilder.UseSerilog((context, lc) =>
+                    lc.MinimumLevel.Warning()
+                      .WriteTo.Console(new JsonFormatter())
+                      .WriteTo.File(new JsonFormatter(), Path.Combine(AppContext.BaseDirectory, $"Logs/log-{DateTime.Now.ToShortDateString()}-{Assembly.GetExecutingAssembly().GetName().Name}.txt"))
+                );
+        }
+    }
+}
---
+++ CoreApiDemoSolution/Models/MoviesRelease.cs
@@ -0,0 +1,8 @@
+﻿namespace CoreApiDemo.Models;
+
+public class MoviesRelease
+{
+    public bool IsAvailable { get; set; }
+    public int NoMoviesRelease { get; set; }
+    public IEnumerable<Models.Movie> Movies { get; set; }
+}
